9f8963b25f14cadc5e3c68979f2eca8551a92dd2
sandy1999 <sandyrockstar19@gmail.com>
2018-09-10 23:51:22 +0530

    removed: commented code

--- insertion_sort.py
+++ insertion_sort.py
@@ -4,7 +4,6 @@ print("Intermediate steps of array sorting")
 for i in range(1,len(array)):
     pivot = array[i]
     j = i-1
-    # print(pivot, j)
     while j >= 0 and pivot < array[j]:
         array[j+1] = array[j]
         j-=1
