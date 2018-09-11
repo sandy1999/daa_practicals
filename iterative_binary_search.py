2daceb389957e450ea2d47237f58296a39ddac97
sandy1999 <sandyrockstar19@gmail.com>
2018-09-11 16:38:05 +0530

    updated: code

--- iterative_binary_search.py
+++ iterative_binary_search.py
@@ -1,16 +1,18 @@
-array = [2 ,3 ,4 ,10 ,40 ,90]
-search_element = int(input("Enter Number to be searched "))
-l = 0
-h = len(array) -1 
-
-while (l < h):
-    mid = int(l + (h-l)/2)
-    
-    if array[mid] == array[mid]:
-        print("Element {} found at position {}".format(search_element, mid))
-        exit()
-    elif array[mid] < search_element:
-        l = mid + 1
-    else:
-        h = mid - 1
-print("Element {} not found in array".format(search_element))
+def binarySearch(alist, item):
+    first = 0
+    last = len(alist)-1
+    found = False
+    while first<=last and not found:
+        midpoint = (first + last)//2
+        if alist[midpoint] == item:
+            print("Element found at postion {}".format(midpoint))
+            exit()
+        else:
+            if item < alist[midpoint]:
+                last = midpoint-1
+            else:
+                first = midpoint+1
+    print("Element not found in array")
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+binarySearch(testlist, 3)
+binarySearch(testlist, 13)
