26c7be6723df549d87a52a8a426455b70d0bdcd8
sandy1999 <sandyrockstar19@gmail.com>
2018-09-18 15:27:27 +0530

    removed: unwanted function

--- iterative_binary_search.py
+++ iterative_binary_search.py
@@ -1,18 +1,16 @@
-def binarySearch(alist, item):
-    first = 0
-    last = len(alist)-1
-    found = False
-    while first<=last and not found:
-        midpoint = (first + last)//2
-        if alist[midpoint] == item:
-            print("Element found at postion {}".format(midpoint))
-            exit()
+array = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+first = 0
+last = len(array)-1
+found = False
+item = int(input("Enter element to be searched in array "))
+while first<=last and not found:
+    midpoint = (first + last)//2
+    if array[midpoint] == item:
+        print("Element found at postion {}".format(midpoint))
+        exit()
+    else:
+        if item < array[midpoint]:
+            last = midpoint-1
         else:
-            if item < alist[midpoint]:
-                last = midpoint-1
-            else:
-                first = midpoint+1
-    print("Element not found in array")
-testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
-binarySearch(testlist, 3)
-binarySearch(testlist, 13)
+            first = midpoint+1
+print("Element not found in array")
